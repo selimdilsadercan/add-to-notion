58e69483abad147a400e8c42b545fca9ec18218c
selimdilsad <selimdilsadercan@gmail.com>
2024-01-21 21:34:39 +0300

    feat: added a soup to find website url from npm via github url

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 from utils import *
 
+
 def get_channel_infos(channel_url):
     request = requests.get(channel_url).text
     soup = BeautifulSoup(request, "lxml")
@@ -11,5 +12,19 @@ def get_channel_infos(channel_url):
     name = soup.body.find("meta", itemprop="name")["content"]
     return img, url, name
 
+
+def get_website_from_npm(url):
+    request1 = requests.get(url).text
+    soup1 = BeautifulSoup(request1, "lxml")
+
+    github_url = "https://" + soup1.body.find("span", id="repository-link").text
+
+    request2 = requests.get(github_url).text
+    soup2 = BeautifulSoup(request2, "lxml")
+
+    website_url = soup2.body.find("h2", text="About").find_next("a", {"class": "text-bold"})["href"]
+    return website_url
+
+
 if __name__ == "__main__":
-    print(get_channel_infos("https://www.youtube.com/@kriss_drummer"))
+    print(get_website_from_npm("https://www.npmjs.com/package/@editorjs/editorjs"))
